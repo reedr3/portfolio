24bc890cc16b5987c487b4515125e1549164cf0d
Ryker Reed <ryker.reed3@gmail.com>
2024-11-21 12:39:56 -0500

    Add ability to build and serve client from server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-})
+app.use(express.static('dist'));
 
 const PORT = 3000;
 app.listen(PORT, () => {
